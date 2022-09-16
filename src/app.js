eebeb5bbd10be3e0f708fb4fed06a812ff93abc9
Tharoxes <thc.huynh@protonmail.com>
2022-09-16 17:04:44 +0200

    css base from repo; header

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+
+function App() {
+    return (
+        <>
+            <Header />  
+            <div className="container">
+                <h1>my App</h1>
+            </div>
+        </>
+
+    )
+}
+
+export default App
